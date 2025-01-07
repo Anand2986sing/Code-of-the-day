2b7d87f64dfb610201424cdc28f6341dc245638c
Anand <94970011+Anand2986sing@users.noreply.github.com>
2025-01-07 18:28:28 +0530

    Add files via upload

---
+++ JS_1_NehaMam.js
@@ -0,0 +1,27 @@
+// NOTE: node JSON, reacjs, viewjs, next js all techonologies use concept of JAVA-Script 
+// to print anything in JS use either [ console.log] or [ write]
+// var and let keyword use to declare variable in JS
+
+//must connect js file in html document to se result
+/*        <script src="JS_1_NehaMam.js"></script>            */
+
+
+
+
+// use document keyword to see direct result on screen
+document.write("Welcome to Javascript series");
+
+
+// use <br> for new line 
+document.write("<br>");
+document.write("Anand ");
+
+
+// we can also use variable to print value
+var name = "Neha Mam";
+document.write(name);
+document.write("<br>");
+
+
+// print date and time 
+document.write(Date());
