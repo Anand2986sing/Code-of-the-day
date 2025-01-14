24856b66508de86d63b634738291ea9d6d6e1bdc
Anand <94970011+Anand2986sing@users.noreply.github.com>
2025-01-14 14:51:48 +0530

    Add files via upload

---
+++ JS_5_Array_NehaMam.js
@@ -0,0 +1,13 @@
+//Array 
+
+const country = ["in", "uk", "us", "caa", "nz"];
+console.log(country[1]);
+
+//Find length of array
+console.log(country.length);
+
+//Print reverse of an array
+console.log(country.reverse());
+
+//find position
+console.log(country.indexOf("caa"));
---
+++ JS_6_PuPo_ShifUnshif_NehaMam.js
@@ -0,0 +1,16 @@
+// push and pop : it is use to remove and add element at the last of the array
+//Shift and unshiftt: it is use to remove and add element at the first of the array
+
+
+
+const country = ["in", "uk", "us", "caa", "nz"];
+country.pop();
+console.log(country);
+country.push("chi");
+console.log(country)
+
+//shift and unshift
+country.shift();
+console.log(country);
+country.unshift("Aus");
+console.log(country)
